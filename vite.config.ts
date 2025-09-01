2c19a523768a244be8eeff437e620e6b2bc732d1
Fabián Gonzalo Artur de la Villarmois <gonzartur@gmail.com>
2025-09-01 13:17:48 +1200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { analyzer } from 'vite-bundle-analyzer'
+// import { analyzer } from 'vite-bundle-analyzer'
 
 export default defineConfig({
   plugins: [
